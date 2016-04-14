c479bfd3d7e8375bfe3ac87350e80939b09af123
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-14 17:07:44 +0900

    Instance model

---
+++ src/main/java/jp/ac/nii/prl/mape/execution/model/Instance.java
@@ -0,0 +1,59 @@
+package jp.ac.nii.prl.mape.execution.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class Instance {
+	
+	@GeneratedValue
+	@JsonIgnore
+	@Id
+	private Long id;
+	
+	@NotEmpty
+	private String ami;
+	
+	@NotEmpty
+	private String instType;
+	
+	@NotEmpty
+	private String securityGroupRef;
+
+	public String getAmi() {
+		return ami;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getInstType() {
+		return instType;
+	}
+
+	public String getSecurityGroupRef() {
+		return securityGroupRef;
+	}
+
+	public void setAmi(String ami) {
+		this.ami = ami;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setInstType(String instType) {
+		this.instType = instType;
+	}
+
+	public void setSecurityGroupRef(String securityGroupRef) {
+		this.securityGroupRef = securityGroupRef;
+	}
+}
