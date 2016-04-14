f54a05e56a1b9e729cbdb17b0f558d55bf289bfc
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-14 16:48:33 +0900

    Import generated Spring project

---
+++ src/main/java/jp/ac/nii/prl/mape/execution/MapeExecutionServiceApplication.java
@@ -0,0 +1,12 @@
+package jp.ac.nii.prl.mape.execution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapeExecutionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapeExecutionServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/jp/ac/nii/prl/mape/execution/MapeExecutionServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package jp.ac.nii.prl.mape.execution;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MapeExecutionServiceApplication.class)
+@WebAppConfiguration
+public class MapeExecutionServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
