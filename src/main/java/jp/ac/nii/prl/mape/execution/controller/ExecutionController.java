99a47dddcfe1dd1985796814fe6d495f7c6980ad
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-18 15:37:29 +0900

    Remove AnsibleProperties from controller

--- src/main/java/jp/ac/nii/prl/mape/execution/controller/ExecutionController.java
+++ src/main/java/jp/ac/nii/prl/mape/execution/controller/ExecutionController.java
@@ -1,6 +1,5 @@
 package jp.ac.nii.prl.mape.execution.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,19 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import jp.ac.nii.prl.mape.execution.model.View;
-import jp.ac.nii.prl.mape.execution.properties.AnsibleProperties;
 
 @RestController
 @Component
 @RequestMapping("/execute")
 public class ExecutionController {
-	
-	private final AnsibleProperties ansibleProperties;
-	
-	@Autowired
-	public ExecutionController(AnsibleProperties ansibleProperties) {
-		this.ansibleProperties = ansibleProperties;
-	}
 	
 	@RequestMapping(method=RequestMethod.POST)
 	public ResponseEntity<?> execute(@RequestBody View view) {
