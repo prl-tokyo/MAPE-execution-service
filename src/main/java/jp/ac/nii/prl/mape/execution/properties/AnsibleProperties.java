ef4ff1bbe052eafe6477279b03e363727119142a
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-18 15:05:16 +0900

    Additional properties
    All represent paths to files:
    - the inventory script (inventory)
    - the playbook to create instances (create)
    - the playbook to terminate instances (terminate)

--- src/main/java/jp/ac/nii/prl/mape/execution/properties/AnsibleProperties.java
+++ src/main/java/jp/ac/nii/prl/mape/execution/properties/AnsibleProperties.java
@@ -8,13 +8,43 @@ import org.springframework.stereotype.Component;
 public class AnsibleProperties {
 
 	private String executable;
+	
+	private String inventory;
+	
+	private String create;
+	
+	private String terminate;
+
+	public String getCreate() {
+		return create;
+	}
 
 	public String getExecutable() {
 		return executable;
 	}
 
+	public String getInventory() {
+		return inventory;
+	}
+
+	public String getTerminate() {
+		return terminate;
+	}
+
+	public void setCreate(String create) {
+		this.create = create;
+	}
+
 	public void setExecutable(String executable) {
 		this.executable = executable;
+	}
+
+	public void setInventory(String inventory) {
+		this.inventory = inventory;
+	}
+
+	public void setTerminate(String terminate) {
+		this.terminate = terminate;
 	}
 	
 	
